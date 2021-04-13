19d670d6d096f4aed850e0d4e0530f8cf096de5e
Prakhar <you@example.com>
2021-04-13 23:25:22 +0530

    aprch 21 socks

---
+++ socks.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        short a=sc.nextShort();
+        short b=sc.nextShort();
+        short c=sc.nextShort();
+        if(a==b|b==c|a==c){System.out.println("yes");}else{System.out.println("no");}
+}
+}
